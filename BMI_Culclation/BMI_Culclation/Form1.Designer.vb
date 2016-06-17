9815f12c31b98db599cfe36c177374bdb0297bd0
nanco2sensei <k.asamura.1101@gmail.com>
2016-06-17 20:34:45 +0900

    DesignChainging

--- BMI_Culclation/BMI_Culclation/Form1.Designer.vb
+++ BMI_Culclation/BMI_Culclation/Form1.Designer.vb
@@ -22,59 +22,24 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(88, 43)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 19)
-        Me.TextBox1.TabIndex = 0
-        Me.TextBox1.Text = "172"
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(88, 106)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 19)
-        Me.TextBox2.TabIndex = 1
-        Me.TextBox2.Text = "57"
-        '
         'Label1
         '
-        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(21, 43)
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(30, 12)
+        Me.Label1.Size = New System.Drawing.Size(284, 12)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "hight"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(17, 109)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(38, 12)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "weight"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(21, 199)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(25, 12)
-        Me.Label3.TabIndex = 5
-        Me.Label3.Text = "BMI"
         '
         'Label4
         '
@@ -84,48 +49,86 @@ Partial Class Form1
         Me.Label4.Size = New System.Drawing.Size(0, 12)
         Me.Label4.TabIndex = 6
         '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TextBox1.Location = New System.Drawing.Point(0, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(284, 19)
+        Me.TextBox1.TabIndex = 10
+        Me.TextBox1.Text = "1.72"
+        '
+        'Label2
+        '
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label2.Location = New System.Drawing.Point(0, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(284, 12)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "weight"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TextBox2.Location = New System.Drawing.Point(0, 43)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(284, 19)
+        Me.TextBox2.TabIndex = 12
+        Me.TextBox2.Text = "57"
+        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(98, 147)
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button1.Location = New System.Drawing.Point(0, 62)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 8
+        Me.Button1.Size = New System.Drawing.Size(284, 23)
+        Me.Button1.TabIndex = 13
         Me.Button1.Text = "Calculation"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label3
+        '
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label3.Location = New System.Drawing.Point(0, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(284, 12)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "BMI"
+        '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(88, 196)
+        Me.TextBox3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TextBox3.Location = New System.Drawing.Point(0, 97)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 19)
-        Me.TextBox3.TabIndex = 9
+        Me.TextBox3.Size = New System.Drawing.Size(284, 19)
+        Me.TextBox3.TabIndex = 15
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(284, 117)
         Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
 
 End Class
--- BMI_Culclation/BMI_Culclation/Form1.vb
+++ BMI_Culclation/BMI_Culclation/Form1.vb
@@ -8,27 +8,25 @@
        
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, TextBox3.TextChanged
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles TextBox3.TextChanged, Button1.Click
         Dim BMI As Double
         Dim hight As Double = Double.Parse(TextBox1.Text)
         Dim weight As Double = Double.Parse(TextBox2.Text)
-        Dim out As String
 
-        BMI = weight / hight * hight
+        BMI = weight / (hight * hight)
 
-        out = BMI.ToString(BMI)
-
-        TextBox3.Text = out
+        TextBox3.Text = Format(BMI, "0.0")
 
     End Sub
 
     Private Sub Label5_Click_1(sender As Object, e As EventArgs)
       
     End Sub
+
 
 
 End Class
